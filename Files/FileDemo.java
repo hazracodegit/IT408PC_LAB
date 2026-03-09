e5b21a047d7d4eb620a8bbeb3b12340d629f2c2b
Dudekula Hazra Bee <dudekulahazrabee@gmail.com>
2026-03-09 20:18:00 +0530

    Create FileDemo.java

---
+++ Files/FileDemo.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class FileDemo{
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		File f = new File("Example.txt");
+		if (f.createNewFile()){
+			System.out.println("File is created.");
+			FileWriter fw = new FileWriter(f);
+			System.out.println("Enter the text you want to enter into the file: ");
+			String str = sc.nextLine();
+			fw.write(str);
+			fw.close();
+			FileReader fr = new FileReader(f);
+			int ch=0;
+			while((ch=fr.read())!=-1){
+				System.out.print((char)ch);
+			}
+			fr.close();
+		}
+		else {
+			System.out.println("File already exists.");
+			FileWriter fw = new FileWriter(f);
+			System.out.println("Enter the text you want to enter into the file: ");
+			String str = sc.nextLine();
+			fw.write(str);
+			fw.close();
+			FileReader fr = new FileReader(f);
+			int ch=0;
+			while((ch=fr.read())!=-1){
+				System.out.print((char)ch);
+			}
+			fr.close();
+		}
+	}
+}
