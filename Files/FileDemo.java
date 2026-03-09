f845ce84d497d05ed9c3bcbf74a3744c5e1af40c
Dudekula Hazra Bee <dudekulahazrabee@gmail.com>
2026-03-09 21:28:53 +0530

    Update FileDemo.java

--- Files/FileDemo.java
+++ Files/FileDemo.java
@@ -7,15 +7,23 @@ import java.io.IOException;
 public class FileDemo{
 	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
-		File f = new File("Example.txt");
-		if (f.createNewFile()){
+		File f = new File("PersonalJava");
+		if (f.exists()){
+			System.out.println("Directory already exists.");
+		}
+		else {
+			f.mkdir();
+			System.out.println("Directory is created.");
+		}
+		File f1 = new File(f,"Example.txt");
+		if (f1.createNewFile()){
 			System.out.println("File is created.");
-			FileWriter fw = new FileWriter(f);
+			FileWriter fw = new FileWriter(f1);
 			System.out.println("Enter the text you want to enter into the file: ");
 			String str = sc.nextLine();
 			fw.write(str);
 			fw.close();
-			FileReader fr = new FileReader(f);
+			FileReader fr = new FileReader(f1);
 			int ch=0;
 			while((ch=fr.read())!=-1){
 				System.out.print((char)ch);
